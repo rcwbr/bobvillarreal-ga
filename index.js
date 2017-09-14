2e4938a7305ab871910dc0b48623768943cd339a
Eric Weber <eric@weber.lol>
2017-09-14 08:30:37 -0700

    create initial analytics server scripts

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+var analyticsUpdater = require('./updateAnalytics.js')
+var vistorCountRefreshRate = 1 / 4 // minutes
+
+analyticsUpdater.updateAnalyticsLoop(vistorCountRefreshRate)
+
+app.get('/', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4567')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', false)
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  res.end(JSON.stringify(analyticsUpdater.analytics))
+})
+
+app.listen(8080, function () {
+  console.log('Server online')
+})
---
+++ updateAnalytics.js
@@ -0,0 +1,48 @@
+var google = require('googleapis')
+var key = require('./serviceAccountInfo.json')
+var analyticsRequestParams = require('./visitorCountQuery.json')
+var ga = google.analytics('v3')
+var analytics = {visitorCount: 0}
+
+var jwtClient = new google.auth.JWT(
+  key.client_email,
+  null,
+  key.private_key,
+  ['https://www.googleapis.com/auth/analytics.readonly'],
+  null
+)
+
+function refreshVisitorCount () {
+  jwtClient.authorize(function (err, tokens) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    analyticsRequestParams.auth = jwtClient
+    ga.data.ga.get(analyticsRequestParams, null, function (err, response) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      console.log(
+        'Refreshed ' +
+        new Date() +
+        ' -- Total sessions: ' +
+        response.totalsForAllResults['ga:sessions']
+      )
+      analytics.visitorCount = response.totalsForAllResults['ga:sessions']
+    })
+  })
+}
+
+module.exports.updateAnalyticsLoop = function (rate) {
+  setInterval(function () {
+    console.log('Refresing visitor count... ')
+    refreshVisitorCount()
+  }, rate * 60 * 1000)
+}
+
+module.exports.analytics = function () {
+  return analytics
+}
